8a648d1ccc5a03f17ac0013567e77fabbd3885bc
Marc Dierckx <marc.dierckx@eduthek.info>
2014-01-17 14:18:15 +0100

    weldinggases

---
+++ WeldingGases/assets/www/cordova_plugins.js
@@ -0,0 +1,46 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.core.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.core.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/Position.js",
+        "id": "org.apache.cordova.core.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.core.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.core.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.inappbrowser/www/InAppBrowser.js",
+        "id": "org.apache.cordova.core.inappbrowser.InAppBrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    }
+]
+});
